c220058bd2d88fef687075db1901995533557fb2
Ricardo <166535715+rllko@users.noreply.github.com>
2025-02-03 23:04:28 +0000

    some database shit

---
+++ HeadHunter/Database/DatabaseInitializer.cs
@@ -0,0 +1,22 @@
+using DbUp;
+
+namespace HeadHunter.Database;
+
+public class DatabaseInitializer(string connectionString)
+{
+    private readonly string _connectionString = "Host=localhost;Port=5432;Pooling=true;Database=auth;User Id=postgres;Password=postgres;";
+
+    public async Task InitializeAsync()
+    {
+        EnsureDatabase.For.PostgresqlDatabase(_connectionString);
+        
+        var upgrader = DeployChanges.To.PostgresqlDatabase(_connectionString)
+            .WithScriptsEmbeddedInAssembly(typeof(DatabaseInitializer).Assembly).LogToConsole().Build();
+        
+        if (upgrader.IsUpgradeRequired())
+        {
+            var result = upgrader.PerformUpgrade();
+        }
+
+    }
+}
