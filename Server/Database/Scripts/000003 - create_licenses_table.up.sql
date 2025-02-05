441c006a5b429ca4aef08d42457620186a0b6c9f
Ricardo <166535715+rllko@users.noreply.github.com>
2025-02-05 23:58:13 +0000

    format

--- Server/Database/Scripts/000003 - create_licenses_table.up.sql
+++ Server/Database/Scripts/000003 - create_licenses_table.up.sql
@@ -1,7 +1,8 @@
-CREATE TABLE licenses (
-                          id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 10 INCREMENT BY 69) PRIMARY KEY,
-                          license UUID DEFAULT gen_random_uuid(),
-                          discord BIGINT REFERENCES discords(discord_id) ON DELETE CASCADE,
-                          hwid BIGINT REFERENCES hwids(id) ON DELETE SET NULL,
-                          creation_date TIMESTAMP DEFAULT NOW()
+CREATE TABLE licenses
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 10 INCREMENT BY 69) PRIMARY KEY,
+    value         UUID      DEFAULT gen_random_uuid(),
+    discord       BIGINT REFERENCES discords (discord_id) ON DELETE CASCADE,
+    hwid          BIGINT REFERENCES hwids (id) ON DELETE SET NULL,
+    creation_date TIMESTAMP DEFAULT NOW()
 );
