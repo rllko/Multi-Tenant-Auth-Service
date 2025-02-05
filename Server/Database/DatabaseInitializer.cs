cf0ba2daeb176ca96c420ffb35ff1eaa0a999a4c
Ricardo <166535715+rllko@users.noreply.github.com>
2025-02-05 17:40:06 +0000

    feat(auth): docker compose for server
     - #26

--- Server/Database/DatabaseInitializer.cs
+++ Server/Database/DatabaseInitializer.cs
@@ -2,21 +2,20 @@ using DbUp;
 
 namespace HeadHunter.Database;
 
-public class DatabaseInitializer(string connectionString)
+public class DatabaseInitializer(string? connectionString)
 {
-    private readonly string _connectionString = "Host=localhost;Port=5432;Pooling=true;Database=auth;User Id=postgres;Password=postgres;";
+    private readonly string _connectionString = "";
 
     public async Task InitializeAsync()
     {
         EnsureDatabase.For.PostgresqlDatabase(_connectionString);
-        
+
         var upgrader = DeployChanges.To.PostgresqlDatabase(_connectionString)
             .WithScriptsEmbeddedInAssembly(typeof(DatabaseInitializer).Assembly).LogToConsole().Build();
-        
+
         if (upgrader.IsUpgradeRequired())
         {
             var result = upgrader.PerformUpgrade();
         }
-
     }
 }
--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,8 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eda23f53c044e880f41d7f9609f820e0b51d80b")]
-
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990180d921fb89cdda908cefb63d68299df0950b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
