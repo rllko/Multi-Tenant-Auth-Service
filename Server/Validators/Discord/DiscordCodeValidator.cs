6e9dc8c19442d1649f04aefafa00e1897da295fc
Ricardo <166535715+rllko@users.noreply.github.com>
2025-02-06 19:51:46 +0000

    models

---
+++ Server/Validators/Discord/DiscordCodeValidator.cs
@@ -0,0 +1,22 @@
+using Authentication.Models.Entities.Discord;
+using FluentValidation;
+
+namespace Authentication.Validators.Discord;
+
+public class DiscordCodeValidator : AbstractValidator<DiscordCode>
+{
+    public DiscordCodeValidator()
+    {
+        RuleFor(x => x.License.DiscordUser)
+            .Null().WithMessage("This License already has a Discord user assigned to it.");
+
+        RuleFor(x => x.CreationTime)
+            .Must(x => BeValid(x, 30))
+            .WithMessage("This code is expired");
+    }
+
+    private static bool BeValid(DateTime time, int minutesUntilExpire)
+    {
+        return time.AddMinutes(minutesUntilExpire).ToUniversalTime() <= DateTime.UtcNow;
+    }
+}
