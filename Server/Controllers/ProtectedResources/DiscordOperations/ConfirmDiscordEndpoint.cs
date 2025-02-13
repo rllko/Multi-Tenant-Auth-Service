50c43855d8d9c26be5f313563945989f399625a5
Ricardo <166535715+rllko@users.noreply.github.com>
2025-02-13 01:59:55 +0000

    Update ConfirmDiscordEndpoint.cs

--- Server/Controllers/ProtectedResources/DiscordOperations/ConfirmDiscordEndpoint.cs
+++ Server/Controllers/ProtectedResources/DiscordOperations/ConfirmDiscordEndpoint.cs
@@ -12,6 +12,32 @@ public class ConfirmDiscordEndpoint
         ILicenseManagerService userManagerService,
         ICodeStorageService codeStorage)
     {
-  
+        var response = new DiscordResponse<string>();
+
+        var Request = httpContext.Request;
+        Request.Form.TryGetValue("code", out var code);
+        Request.Form.TryGetValue("discord_id", out var discordId);
+
+        if (string.IsNullOrEmpty(code))
+        {
+            response.Error = "Invalid Params!";
+            return Results.Json(response);
+        }
+
+        var userFromCode = codeStorage.GetDiscordCode(code.ToString());
+
+        if (userFromCode == null)
+        {
+            response.Error = "Key is invalid.";
+            return Results.Json(response);
+        }
+
+        long.TryParse(discordId, out var discord);
+
+        //userFromCode.License.DiscordUser = discord;
+        // var updatedUser = await userManagerService.ConfirmUserRegistrationAsync(userFromCode);
+
+        response.Result = "Sucess!";
+        return Results.Json(response);
     }
 }
