67a53f78868d7bb461f4a6e1c31c0752931457d8
Ricardo <166535715+rllko@users.noreply.github.com>
2025-02-14 20:11:56 +0000

    feat(auth): docker service to generate security keys
     #39

--- Server/Misc/EncodingFunctions.cs
+++ Server/Misc/EncodingFunctions.cs
@@ -1,8 +1,5 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
-using Authentication.Models.Entities;
-using Authentication.Services;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Authentication.Common;
 
@@ -29,32 +26,5 @@ public static class EncodingFunctions
         }
 
         return result.ToString();
-    }
-
-
-    public static SecurityTokenDescriptor? GenerateSecurityTokenDescriptor(License user, DevKeys keys)
-    {
-        // const int ExpireDays = 7;
-        //
-        // if (user is null) return null;
-        //
-        // // Create token to send to the user
-        // return new SecurityTokenDescriptor
-        // {
-        //     Audience = IdentityData.Audience,
-        //     Issuer = IdentityData.Issuer,
-        //     IssuedAt = DateTime.Now,
-        //     Expires = user.LastToken!.Value.AddDays(ExpireDays),
-        //     Claims = new Dictionary<string, object>
-        //     {
-        //         [JwtRegisteredClaimNames.Jti] = user.License,
-        //         [JwtRegisteredClaimNames.AuthTime] = user.LastToken,
-        //         ["PersistenceToken"] = user.PersistentToken!,
-        //         ["Hwid"] = user.Hwid!
-        //     },
-        //     SigningCredentials =
-        //         new SigningCredentials(new RsaSecurityKey(keys.RsaSignKey), SecurityAlgorithms.RsaSha256)
-        // };º
-        return null;
     }
 }
