a8b66bd2ef8b77be38b01de31d0117a463498817
Ricardo <166535715+rllko@users.noreply.github.com>
2025-04-13 22:37:22 +0100

    a

---
+++ MigrationUtility/Program.cs
@@ -0,0 +1,12 @@
+using Authentication.Database;
+using MigrationUtility.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(_ => new DatabaseInitializer(
+    Environment.GetEnvironmentVariable("DATABASE_URL")));
+
+var app = builder.Build();
+
+var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+await databaseInitializer.InitializeAsync();
