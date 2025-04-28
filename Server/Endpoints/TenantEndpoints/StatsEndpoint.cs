90da0918b7348ae6af9984169c029472160ec3c6
rllko <ric.fon@ua.pt>
2025-04-28 01:50:42 +0100

    a

---
+++ Server/Endpoints/TenantEndpoints/StatsEndpoint.cs
@@ -0,0 +1,24 @@
+using Authentication.Models;
+using Authentication.RequestProcessors;
+using FastEndpoints;
+
+namespace Authentication.Endpoints.Authentication.TenantAuthentication;
+
+public class StatsEndpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Claims("access_token");
+        Get("/tenants/test");
+        Throttle(
+            10,
+            60
+        );
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var context = HttpContext.User.Claims.First(x => x.Type == "access_token");
+        await SendOkAsync(new { yuh = context.Value }, ct);
+    }
+}
